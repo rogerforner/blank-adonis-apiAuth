cf3fc6943ab0007520ea86c8d6968f194937c26a
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-02-12 11:00:27 +0100

    Controlador para los registros de usuarios

---
+++ app/Controllers/Http/Auth/RegisterController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const Persona = use("Persona");
+
+class RegisterController {
+  async store ({ request, response }) {
+    const payload = request.only([
+      'username', 'email', 'password', 'password_confirmation']
+    )
+
+    const user = await Persona.register(payload)
+
+    return response.ok({
+      user,
+      status: 200,
+      message: 'Account created successfully',
+    })
+  }
+}
+
+module.exports = RegisterController
