6936036dd13e7ba84f75fd04677010a011a93432
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-02-12 11:44:03 +0100

    ; Punto y Coma
    Siempre utilizar punto y coma para evitar bugs. Que sí, JavaScript es bonito sin puntos y comas, pero evitaremos bugs :alien:

--- app/Controllers/Http/Auth/RegisterController.js
+++ app/Controllers/Http/Auth/RegisterController.js
@@ -6,16 +6,16 @@ class RegisterController {
   async store ({ request, response }) {
     const payload = request.only([
       'username', 'email', 'password', 'password_confirmation']
-    )
+    );
 
-    const user = await Persona.register(payload)
+    const user = await Persona.register(payload);
 
     return response.ok({
       user,
       status: 200,
       message: 'Account created successfully',
-    })
+    });
   }
 }
 
-module.exports = RegisterController
+module.exports = RegisterController;
