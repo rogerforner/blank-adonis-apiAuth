e86c9e7325db271ea7681e41b5abaedd61c1bb46
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-02-13 23:06:56 +0100

    Controlador de verificación de email + ruta

--- app/Controllers/Http/Auth/RegisterVerifyEmailController.js
+++ app/Controllers/Http/Auth/RegisterVerifyEmailController.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const Encryption = use('Encryption');
+const Persona    = use('Persona');
+
 class RegisterVerifyEmailController {
+  async verify ({ request, response }) {
+    const token = Encryption.base64Decode(request.input('token'));
+    
+    await Persona.verifyEmail(token);
+
+    return response.ok({
+      status: 200,
+      message: 'Account validated successfully',
+    });
+  }
 }
 
-module.exports = RegisterVerifyEmailController
+module.exports = RegisterVerifyEmailController;
--- start/routes.js
+++ start/routes.js
@@ -21,4 +21,5 @@ Route.get('/', () => {
 
 Route.group(() => {
   Route.post('register', 'RegisterController.store').validator('AuthRegister');
+  Route.post('verify-email', 'RegisterVerifyEmailController.verify');
 }).prefix('auth').namespace('Auth');
