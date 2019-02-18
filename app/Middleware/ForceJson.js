21bda8847c43a112d9cf22a6d3803e6897f6b624
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-02-18 19:49:16 +0100

    Middleware ForzarJson OK :+1:
    Para que se visualizen las "vistas" en json (HttpException) se tiene que modificar la variable de .env NODE_ENV a production

--- app/Middleware/ForceJson.js
+++ app/Middleware/ForceJson.js
@@ -2,7 +2,7 @@
 
 class ForceJson {
   async handle ({ request }, next) {
-    request.request.headers.accept = 'application/json';
+    request.request.headers['accept'] = 'application/json';
     await next();
   }
 }
