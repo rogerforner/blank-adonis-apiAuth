c014994912d8b9913182ab90510c414d238e7d45
Roger Forner Fabre <sir.rogerforner@gmail.com>
2019-02-20 21:34:50 +0100

    Env APP_URL :ok:
    Como no podemos insertar un getter para obtener el valor de una variable del env dentro de otro getter, creamos un condicional con el que poder seleccionar la URL correcta :smiley_cat:

--- app/Listeners/Auth/SendEmailForgotPassword.js
+++ app/Listeners/Auth/SendEmailForgotPassword.js
@@ -7,10 +7,18 @@ const Mail       = use('Mail');
 const SendEmailForgotPassword = exports = module.exports = {}
 
 SendEmailForgotPassword.method = async ({ user, token }) => {
+  let appUrl;
+
+  if (Env.get('APP_URL_VIEW')) {
+    appUrl = Env.get('APP_URL_VIEW');
+  } else {
+    appUrl = Env.get('APP_URL');
+  }
+
   Mail.send('emails.auth.forgot-password', {
     user,
     token: Encryption.base64Encode(token),
-    url  : Env.get('APP_URL_VIEW', Env.get('APP_URL'))
+    url  : appUrl
   },
   (message) => {
     message.to(user.email).from(Env.get('MAIL_FROM')).subject(Env.get('APP_NAME') + ' | Restore password');
--- app/Listeners/Auth/SendEmailVerifyEmail.js
+++ app/Listeners/Auth/SendEmailVerifyEmail.js
@@ -7,10 +7,18 @@ const Mail       = use('Mail');
 const SendEmailVerifyEmail = exports = module.exports = {}
 
 SendEmailVerifyEmail.method = async ({ user, token }) => {
+  let appUrl;
+
+  if (Env.get('APP_URL_VIEW')) {
+    appUrl = Env.get('APP_URL_VIEW');
+  } else {
+    appUrl = Env.get('APP_URL');
+  }
+
   Mail.send('emails.auth.verify-email', {
     user,
     token: Encryption.base64Encode(token),
-    url  : Env.get('APP_URL_VIEW', Env.get('APP_URL'))
+    url  : appUrl
   },
   (message) => {
     message.to(user.email).from(Env.get('MAIL_FROM')).subject(Env.get('APP_NAME') + ' | Email verification');
